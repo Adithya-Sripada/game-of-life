f536a7a642619de033c00cc35efa4a896d7f87f8
Adithya-Sripada <sripada159@gmail.com>
2026-09-09 18:58:02 -0700

    Step 2 complete

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import random
 
+
+class Colors:
+    DEAD = '\033[31m'
+    ALIVE = '\033[92m'
+    RESET = '\033[0m'
+
 def null_state(width, height):
     return [[0 for x in range(width)] for y in range(height)]
 
@@ -10,7 +16,18 @@ def random_state(width, height):
         for c in range(width):
             board[r][c] = round(n)
             n = random.random()
-
     return board
 
-print(random_state(2, 5))
+def render(board):
+    for row in board:
+        for col in row:
+            if col:
+                print(Colors.ALIVE + 'A' + Colors.RESET, end=' ')
+            else:
+                print(Colors.DEAD + 'D' + Colors.RESET,end=' ')
+        print('\n')
+    print(Colors.RESET)
+
+
+if __name__ == '__main__':
+    render(random_state(30, 20))
